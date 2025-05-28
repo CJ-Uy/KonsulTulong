2c0a7e529c2696cd0a2cfd5f76e13e4f63224170
Remy <75460325+CJ-Uy@users.noreply.github.com>
2025-05-29 03:13:29 +0800

    formatted quotes

--- src/routes/demo/paraglide/+page.svelte
+++ src/routes/demo/paraglide/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { setLocale } from '$lib/paraglide/runtime';
-	import { page } from '$app/state';
-	import { goto } from '$app/navigation';
-	import { m } from '$lib/paraglide/messages.js';
+	import { setLocale } from "$lib/paraglide/runtime";
+	import { page } from "$app/state";
+	import { goto } from "$app/navigation";
+	import { m } from "$lib/paraglide/messages.js";
 </script>
 
-<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<h1>{m.hello_world({ name: "SvelteKit User" })}</h1>
 <div>
-	<button onclick={() => setLocale('en')}>en</button>
-	<button onclick={() => setLocale('fil')}>fil</button>
-	<button onclick={() => setLocale('ceb')}>ceb</button>
-	<button onclick={() => setLocale('hil')}>hil</button>
-	<button onclick={() => setLocale('war')}>war</button>
+	<button onclick={() => setLocale("en")}>en</button>
+	<button onclick={() => setLocale("fil")}>fil</button>
+	<button onclick={() => setLocale("ceb")}>ceb</button>
+	<button onclick={() => setLocale("hil")}>hil</button>
+	<button onclick={() => setLocale("war")}>war</button>
 </div>
 <p>
 	If you use VSCode, install the <a
