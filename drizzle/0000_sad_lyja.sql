020e0416eed2d440bf32aeb8dea0f1b4747711af
Remy <75460325+CJ-Uy@users.noreply.github.com>
2025-06-20 19:47:37 +0800

    better-auth added

--- drizzle/0000_sad_lyja.sql
+++ drizzle/0000_sad_lyja.sql
@@ -1,3 +1,19 @@
+CREATE TABLE "account" (
+	"id" text PRIMARY KEY NOT NULL,
+	"account_id" text NOT NULL,
+	"provider_id" text NOT NULL,
+	"user_id" text NOT NULL,
+	"access_token" text,
+	"refresh_token" text,
+	"id_token" text,
+	"access_token_expires_at" timestamp,
+	"refresh_token_expires_at" timestamp,
+	"scope" text,
+	"password" text,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "clinics" (
 	"id" varchar(24) PRIMARY KEY NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
@@ -21,6 +37,18 @@ CREATE TABLE "responses" (
 	"values" jsonb NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"token" text NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	"ip_address" text,
+	"user_agent" text,
+	"user_id" text NOT NULL,
+	CONSTRAINT "session_token_unique" UNIQUE("token")
+);
+--> statement-breakpoint
 CREATE TABLE "templates" (
 	"id" varchar(24) PRIMARY KEY NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
@@ -40,11 +68,25 @@ CREATE TABLE "users" (
 	"deleted_at" timestamp with time zone,
 	"is_active" boolean DEFAULT true NOT NULL,
 	"first_name" varchar,
-	"last_name" varchar
+	"last_name" varchar,
+	"email" text NOT NULL,
+	"email_verified" boolean NOT NULL,
+	"image" text,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
-DROP TABLE "user" CASCADE;--> statement-breakpoint
+CREATE TABLE "verification" (
+	"id" text PRIMARY KEY NOT NULL,
+	"identifier" text NOT NULL,
+	"value" text NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	"created_at" timestamp,
+	"updated_at" timestamp
+);
+--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "clinics" ADD CONSTRAINT "clinics_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "clinics" ADD CONSTRAINT "clinics_active_template_id_templates_id_fk" FOREIGN KEY ("active_template_id") REFERENCES "public"."templates"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "responses" ADD CONSTRAINT "responses_clinic_id_clinics_id_fk" FOREIGN KEY ("clinic_id") REFERENCES "public"."clinics"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "responses" ADD CONSTRAINT "responses_template_id_templates_id_fk" FOREIGN KEY ("template_id") REFERENCES "public"."templates"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "responses" ADD CONSTRAINT "responses_template_id_templates_id_fk" FOREIGN KEY ("template_id") REFERENCES "public"."templates"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
--- src/db/scripts/populate.ts
+++ src/db/scripts/populate.ts
@@ -1,11 +1,23 @@
 import { db } from "..";
 import { users } from "../schema";
+import { faker } from "@faker-js/faker";
 
 async function main() {
-	await db.insert(users).values({
-		firstName: "Hello",
-		lastName: "World"
-	});
+
+    console.log("Adding users...")
+    for (let i = 0; i < 10; i++) {
+        await db.insert(users).values({
+            firstName: faker.person.firstName,
+            lastName: faker.person.lastName
+        });
+    }
+    console.log("Completed Adding Users");
+    
+    console.log("Adding Clinic");
+
+
+	await db.$pool.end();
+	console.log("Closed Connection With Database");
 }
 
 main();
