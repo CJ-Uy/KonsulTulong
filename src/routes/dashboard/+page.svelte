d0020912dbb93293169a758ad83d22868812b7fb
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2025-06-26 17:39:05 +0800

    Update +page.svelte

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
 	import data from "./data.js";
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-	import AppSidebar from "$lib/components/app-sidebar.svelte";
-	import SiteHeader from "$lib/components/site-header.svelte";
 	import DataTable from "$lib/components/data-table.svelte";
 </script>
 
-<Sidebar.Provider
-	style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
->
-	<AppSidebar variant="inset" />
-	<Sidebar.Inset>
-		<SiteHeader />
-		<div class="flex flex-1 flex-col">
-			<div class="@container/main flex flex-1 flex-col gap-2">
-				<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-					<DataTable {data} />
-				</div>
-			</div>
+<div class="flex flex-1 flex-col">
+	<div class="@container/main flex flex-1 flex-col gap-2">
+		<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+			<DataTable {data} />
 		</div>
-	</Sidebar.Inset>
-</Sidebar.Provider>
+	</div>
+</div>
